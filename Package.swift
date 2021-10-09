ba7ea95d27c7d0247a6b7bdba7abffe635c93a68
tid <tid.develop@gmail.com>
2021-10-10 02:21:54 +0900

    WIP

--- Package.swift
+++ Package.swift
@@ -1,19 +1,22 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Sample",
+    products: [
+        .executable(name: "sample", targets: ["Sample"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/tid-kijyun/SampleLib.git", branch: "main"),
+        .package(url: "https://github.com/tid-kijyun/SampleLib.git", .branch("main")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
+        .target(
             name: "Sample",
             dependencies: ["SampleLib"],
             resources: [
