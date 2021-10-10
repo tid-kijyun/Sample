e651cad2df47b2114d5edb50946c6fe0f5979cde
tid <tid.develop@gmail.com>
2021-10-10 15:43:22 +0900

    WIP

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/tid-kijyun/SampleLib.git", from: "0.0.2"),
+        .package(url: "https://github.com/tid-kijyun/SampleLib.git", from: "0.0.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
